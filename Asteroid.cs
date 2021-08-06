5f9e76e2e7c31acdcbc0ea3d947a71a74d9b4ddb
Zak Morrison <zakmorrison@mmc.school.nz>
2021-08-06 15:00:22 +1200

    Asteroid and spaceship drawn in the game area (Panel)

--- Asteroid.cs
+++ Asteroid.cs
@@ -9,6 +9,28 @@ namespace Asteroid_Dodge
 {
     class Asteroid
     {
-        
+        // declare fields to use in the class
+        public int x, y, width, height;//variables for the rectangle
+        public Image asteroidImage;//variable for the Asteroid's image
+
+        public Rectangle asteroidRec;//variable for a rectangle to place our image in
+        // Methods for the Asteroid class
+        public void DrawPlanet(Graphics g)
+        {
+            asteroidRec = new Rectangle(x, y, width, height);
+            g.DrawImage(asteroidImage, asteroidRec);
+        }
+
+        //Create a constructor (initialises the values of the fields)
+        public Asteroid(int spacing)
+        {
+            x = spacing;
+            y = -50;
+            width = 20;
+            height = 50;
+            //AsteroidImage contains the asteroid.png image
+            asteroidImage = Properties.Resources.Asteroid;
+            asteroidRec = new Rectangle(x, y, width, height);
+        }
     }
 }
